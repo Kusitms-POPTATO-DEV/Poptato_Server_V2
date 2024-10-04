ca9a49fd1c51104fe495bd6ed3e7682f825e84ec
kang yeon joo <rkdduswndi@naver.com>
2024-10-04 11:21:41 +0900

    chore: spring 및 .gitignore 초기화

---
+++ src/main/java/server/poptato/PoptatoApplication.java
@@ -0,0 +1,14 @@
+package server.poptato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoptatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoptatoApplication.class, args);
+        System.out.println("Hello Poptato Server Team !!");
+    }
+
+}
