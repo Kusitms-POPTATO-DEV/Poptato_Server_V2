08e1fe3f85721a5d5224c017682af8166196e4e1
kang yeon joo <rkdduswndi@naver.com>
2024-10-14 01:37:54 +0900

    chore: todo 엔티티 생성

---
+++ src/main/java/server/poptato/todo/domain/entity/Todo.java
@@ -0,0 +1,41 @@
+package server.poptato.todo.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import server.poptato.todo.domain.value.TodayStatus;
+import server.poptato.todo.domain.value.Type;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Todo{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Type type;
+    @NotNull
+    private String content;
+    private LocalDate deadline;
+    @NotNull
+    private boolean isBookmark;
+    private LocalDate todayDate;
+    @Enumerated(EnumType.STRING)
+    private TodayStatus todayStatus;
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+}
