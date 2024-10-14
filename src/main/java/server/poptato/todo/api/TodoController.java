0ed7b82181d32f0345edf7cc14af3f45c5fbb0f0
kang yeon joo <rkdduswndi@naver.com>
2024-10-14 16:43:37 +0900

    feat: todoController 구현

--- src/main/java/server/poptato/todo/api/TodoController.java
+++ src/main/java/server/poptato/todo/api/TodoController.java
@@ -2,18 +2,23 @@ package server.poptato.todo.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import server.poptato.global.response.BaseResponse;
 import server.poptato.todo.api.response.TodayListResponseDto;
 import server.poptato.todo.application.TodoService;
+import server.poptato.user.resolver.UserId;
 
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
     private final TodoService todoService;
     @GetMapping("/todays")
-    public BaseResponse<TodayListResponseDto> getTodayList(){
-        TodayListResponseDto todayListResponse = todoService.getTodayList();
+    public BaseResponse<TodayListResponseDto> getTodayList(
+            @UserId Long userId,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "8") int size){
+        TodayListResponseDto todayListResponse = todoService.getTodayList(userId, page, size);
         return new BaseResponse<>(todayListResponse);
     }
 }
--- src/main/java/server/poptato/todo/application/TodoService.java
+++ src/main/java/server/poptato/todo/application/TodoService.java
@@ -1,9 +1,11 @@
 package server.poptato.todo.application;
 
+import org.springframework.stereotype.Service;
 import server.poptato.todo.api.response.TodayListResponseDto;
 
+@Service
 public class TodoService {
-    public TodayListResponseDto getTodayList() {
+    public TodayListResponseDto getTodayList(long userId, int page, int size) {
         return null;
     }
 }
