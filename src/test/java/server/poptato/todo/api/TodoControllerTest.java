b94d2a0648210cba0162c5b83c1f6d8460551c8c
kang yeon joo <rkdduswndi@naver.com>
2024-10-14 18:17:16 +0900

    fix: redisTemplate을 mocking

--- src/test/java/server/poptato/todo/api/TodoControllerTest.java
+++ src/test/java/server/poptato/todo/api/TodoControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -27,6 +28,8 @@ public class TodoControllerTest {
     private TodoService todoService;
     @Autowired
     private JwtService jwtService;
+    @MockBean
+    private RedisTemplate<String, String> redisTemplate;
     private String accessToken;
     private final String userId = "1";
 
