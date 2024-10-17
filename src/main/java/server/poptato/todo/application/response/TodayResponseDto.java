636ba19d985b2a93c9a99868463909a692999c43
kang yeon joo <rkdduswndi@naver.com>
2024-10-17 16:04:19 +0900

    fix: 할 일 리스트 조회에 마감기한 데이터 응답 추가

--- src/main/java/server/poptato/todo/application/response/TodayResponseDto.java
+++ src/main/java/server/poptato/todo/application/response/TodayResponseDto.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import server.poptato.todo.domain.entity.Todo;
 import server.poptato.todo.domain.value.TodayStatus;
 
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 @Getter
@@ -12,19 +13,21 @@ public class TodayResponseDto {
     String content;
     TodayStatus todayStatus;
     boolean isBookmark;
-    Integer deadline;
+    Integer dDay;
+    LocalDate deadline;
 
     public TodayResponseDto(Todo todo) {
         this.todoId = todo.getId();
         this.content = todo.getContent();
         this.todayStatus = todo.getTodayStatus();
         this.isBookmark = todo.isBookmark();
+        this.deadline = todo.getDeadline();
 
         if (todo.getDeadline() != null && todo.getTodayDate() != null) {
-            this.deadline = (int) ChronoUnit.DAYS.between(todo.getTodayDate(), todo.getDeadline());
+            this.dDay = (int) ChronoUnit.DAYS.between(todo.getTodayDate(), todo.getDeadline());
         } else {
             // 마감 기한을 계산할 수 없는 경우 NULL
-            this.deadline = null;
+            this.dDay = null;
         }
     }
 }
