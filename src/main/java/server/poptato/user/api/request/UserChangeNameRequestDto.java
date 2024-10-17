28ba209a20dde99d5259612d20c10b1ec23abaf0
pkl0912 <pklpkl0912@gmail.com>
2024-10-17 12:49:09 +0900

    feat: 이름 변경 service 추가

---
+++ src/main/java/server/poptato/user/api/request/UserChangeNameRequestDto.java
@@ -0,0 +1,14 @@
+package server.poptato.user.api.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserChangeNameRequestDto {
+    String newName;
+}
