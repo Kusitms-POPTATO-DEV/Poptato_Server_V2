227eb8b9cd996538428ff711175fd94d67a55444
kang yeon joo <rkdduswndi@naver.com>
2024-11-05 17:27:10 +0900

    refactor: 스케줄러 클래스 메소드 추출

--- src/main/java/server/poptato/todo/application/TodoScheduler.java
+++ src/main/java/server/poptato/todo/application/TodoScheduler.java
@@ -18,41 +18,48 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class TodoScheduler {
     private final TodoRepository todoRepository;
-    @Scheduled(cron = "0 0 0 * * *") // 매일 자정에 실행
+    @Scheduled(cron = "0 0 0 * * *")
     @Transactional
     public void updateTodoType() {
-        // 1. TODAY 상태에서 INCOMPLETE인 할 일들을 YESTERDAY로 전환 (사용자별로 처리)
-        Map<Long, List<Todo>> todayIncompleteTodosByUser = todoRepository.findByTypeAndTodayStatus(Type.TODAY, TodayStatus.INCOMPLETE)
+        List<Long> updatedTodoIds = new ArrayList<>();
+        Map<Long, List<Todo>> userIdAndIncompleteTodaysMap = changeIncompleteTodayToYesterday(updatedTodoIds);
+        List<Todo> yesterdayIncompleteTodos = changeYesterdayToBacklog(updatedTodoIds);
+        save(userIdAndIncompleteTodaysMap, yesterdayIncompleteTodos);
+    }
+
+    private Map<Long, List<Todo>> changeIncompleteTodayToYesterday(List<Long> updatedTodoIds) {
+        Map<Long, List<Todo>> userIdAndIncompleteTodaysMap = todoRepository.findByTypeAndTodayStatus(Type.TODAY, TodayStatus.INCOMPLETE)
                 .stream()
-                .collect(Collectors.groupingBy(Todo::getUserId)); // 사용자별로 그룹화
+                .collect(Collectors.groupingBy(Todo::getUserId));
 
-        List<Long> updatedTodoIds = new ArrayList<>();  // YESTERDAY로 전환된 할일들의 ID 저장
-
-        todayIncompleteTodosByUser.forEach((userId, todos) -> {
+        userIdAndIncompleteTodaysMap.forEach((userId, todos) -> {
             Integer minBacklogOrder = todoRepository.findMinBacklogOrderByUserIdOrZero(userId);
             int startingOrder = minBacklogOrder - 1;
 
             for (Todo todo : todos) {
                 todo.setType(Type.YESTERDAY);
                 todo.setBacklogOrder(startingOrder--);
-                updatedTodoIds.add(todo.getId());  // YESTERDAY로 전환된 항목의 ID 추가
+                updatedTodoIds.add(todo.getId());
             }
         });
+        return userIdAndIncompleteTodaysMap;
+    }
 
-        // 2. YESTERDAY 상태에서 INCOMPLETE인 할 일들을 BACKLOG로 전환 (BacklogOrder 유지)
-        // 첫 번째 단계에서 YESTERDAY로 전환된 항목을 제외하고 처리
+    private List<Todo> changeYesterdayToBacklog(List<Long> updatedTodoIds) {
         List<Todo> yesterdayIncompleteTodos = todoRepository.findByTypeAndTodayStatus(Type.YESTERDAY, TodayStatus.INCOMPLETE)
                 .stream()
-                .filter(todo -> !updatedTodoIds.contains(todo.getId()))  // 이미 YESTERDAY로 바뀐 항목 제외
+                .filter(todo -> !updatedTodoIds.contains(todo.getId()))
                 .collect(Collectors.toList());
 
         yesterdayIncompleteTodos.forEach(todo -> {
             todo.setType(Type.BACKLOG);
             todo.setTodayStatus(null);
         });
+        return yesterdayIncompleteTodos;
+    }
 
-        // 3. 저장
-        for (Todo todo : todayIncompleteTodosByUser.values().stream().flatMap(List::stream).collect(Collectors.toList())) {
+    private void save(Map<Long, List<Todo>> userIdAndIncompleteTodaysMap, List<Todo> yesterdayIncompleteTodos) {
+        for (Todo todo : userIdAndIncompleteTodaysMap.values().stream().flatMap(List::stream).collect(Collectors.toList())) {
             todoRepository.save(todo);
         }
         for (Todo todo : yesterdayIncompleteTodos) {
