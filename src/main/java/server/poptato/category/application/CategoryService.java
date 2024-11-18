33662e91c8d5d5a0db1aaa2b1fb3d2b9ae20ff93
kang yeon joo <rkdduswndi@naver.com>
2024-11-19 01:11:27 +0900

    refactor: CategoryValidator 구현

--- src/main/java/server/poptato/category/application/CategoryService.java
+++ src/main/java/server/poptato/category/application/CategoryService.java
@@ -12,6 +12,7 @@ import server.poptato.category.application.response.CategoryResponseDto;
 import server.poptato.category.domain.entity.Category;
 import server.poptato.category.domain.repository.CategoryRepository;
 import server.poptato.category.exception.CategoryException;
+import server.poptato.category.validator.CategoryValidator;
 import server.poptato.emoji.domain.repository.EmojiRepository;
 import server.poptato.emoji.validator.EmojiValidator;
 import server.poptato.user.validator.UserValidator;
@@ -28,6 +29,7 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final UserValidator userValidator;
     private final EmojiValidator emojiValidator;
+    private final CategoryValidator categoryValidator;
     private final EmojiRepository emojiRepository;
     public CategoryCreateResponseDto createCategory(Long userId, CategoryCreateUpdateRequestDto request) {
         userValidator.checkIsExistUser(userId);
@@ -59,21 +61,14 @@ public class CategoryService {
     public void updateCategory(Long userId, Long categoryId, CategoryCreateUpdateRequestDto updateRequestDto) {
         userValidator.checkIsExistUser(userId);
         emojiValidator.checkIsExistEmoji(updateRequestDto.emojiId());
-        Category category = validateAndReturnCategory(userId, categoryId);
+        Category category = categoryValidator.validateAndReturnCategory(userId, categoryId);
         category.update(updateRequestDto);
         categoryRepository.save(category);
     }
 
-    private Category validateAndReturnCategory(Long userId, Long categoryId) {
-        Category findCategory = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new CategoryException(CATEGORY_NOT_EXIST));
-        if (findCategory.getUserId() != userId) throw new CategoryException(CATEGORY_USER_NOT_MATCH);
-        return findCategory;
-    }
-
     public void deleteCategory(Long userId, Long categoryId) {
         userValidator.checkIsExistUser(userId);
-        Category category = validateAndReturnCategory(userId, categoryId);
+        Category category = categoryValidator.validateAndReturnCategory(userId, categoryId);
         categoryRepository.delete(category);
     }
 }
