cee84d54bdba1623bc4137d2a0c10f6f5b6c79ed
pkl0912 <pklpkl0912@gmail.com>
2024-11-21 21:55:20 +0900

    chore: repository 작성

---
+++ src/main/java/server/poptato/deleteReason/domain/repository/DeleteReasonRepository.java
@@ -0,0 +1,10 @@
+package server.poptato.deleteReason.domain.repository;
+
+import server.poptato.deleteReason.domain.entity.DeleteReason;
+
+import java.util.List;
+
+public interface DeleteReasonRepository {
+    DeleteReason save(DeleteReason deleteReason);
+    List<DeleteReason> findAll();
+}
