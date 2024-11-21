21c5f7f0165bf485eaa31f7f05690df48b7f4b85
pkl0912 <pklpkl0912@gmail.com>
2024-11-21 21:56:20 +0900

    chore: test code 작성

--- src/test/java/server/poptato/user/api/UserControllerTest.java
+++ src/test/java/server/poptato/user/api/UserControllerTest.java
@@ -77,6 +77,7 @@ class UserControllerTest {
     void deleteUser_Success() throws Exception {
         //given & when & then
         mockMvc.perform(MockMvcRequestBuilders.delete("/user")
+                        .content("{\"reasons\": [\"NOT_USED_OFTEN\", \"MISSING_FEATURES\"], \"userInputReason\": \"서비스가 복잡해요\"}")
                         .header("Authorization", "Bearer " + accessToken))
                 .andExpect(status().isOk())
                 .andDo(print());
@@ -99,6 +100,7 @@ class UserControllerTest {
 
         //when & then
         mockMvc.perform(MockMvcRequestBuilders.delete("/user")
+                        .content("{\"reasons\": [\"NOT_USED_OFTEN\", \"MISSING_FEATURES\"], \"userInputReason\": \"서비스가 복잡해요\"}")
                         .header("Authorization", "Bearer " + invalidToken))
                 .andExpect(status().isUnauthorized())
                 .andDo(print());
